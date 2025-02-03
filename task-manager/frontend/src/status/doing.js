fd54649b999d52f2fffea50cc06d5a80ec7c471b
Samuel Costa <samuel.costa@ccc.ufcg.edu.br>
2025-02-03 01:43:16 -0300

    feat(frontend): styling status columns.

--- task-manager/frontend/src/status/doing.js
+++ task-manager/frontend/src/status/doing.js
@@ -2,8 +2,11 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { io } from 'socket.io-client';
 
-import { Box, Paper } from '@mui/material';
+import { Avatar, List, ListItemAvatar,
+  ListItemButton, ListItemText, Paper
+} from '@mui/material';
 import { orange } from '@mui/material/colors';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
 
 const socket = io('http://localhost:5000', {
   transports: ['websocket'],
@@ -34,12 +37,45 @@ function Doing() {
   }, []);
 
   return (
-    <Paper elevation={3} sx={{ p: 2, gap: 2, width: { md: '100%' } }}>
-      {tasks.map(task => (
-        <Box sx={{ bgcolor: orange[500] }}>
-          {task.title}
-        </Box>
-      ))}
+    <Paper
+      elevation={3}
+      sx={{
+        p: 2,
+        minHeight: '60dvh',
+        width: { md: '100%' } 
+      }}
+    >
+      <List sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+        {tasks.map((task, index) => (
+          <ListItemButton
+            key={index}
+            sx={{
+              bgcolor: orange[500],
+              color: 'white',
+              p: 2,
+
+              '&:hover': {
+                bgcolor: orange[400]
+              }
+            }}
+          >
+            <ListItemAvatar>
+              <Avatar sx={{ bgcolor: orange[700] }}>
+                <AccessTimeIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText
+              primary={task.title}
+              primaryTypographyProps={{
+                noWrap: true,
+                style: { overflow: 'hidden', textOverflow: 'ellipsis' }
+              }}
+            >
+              {task.title}
+            </ListItemText>
+          </ListItemButton>
+        ))}
+      </List>
     </Paper>
   )
 }
--- task-manager/frontend/src/status/pending.js
+++ task-manager/frontend/src/status/pending.js
@@ -2,8 +2,11 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { io } from 'socket.io-client';
 
-import { Box, Paper } from '@mui/material';
+import { Avatar, List, ListItemAvatar,
+  ListItemButton, ListItemText, Paper
+} from '@mui/material';
 import { red } from '@mui/material/colors';
+import PendingIcon from '@mui/icons-material/Pending';
 
 const socket = io('http://localhost:5000', {
   transports: ['websocket'],
@@ -38,16 +41,41 @@ function Pending() {
       elevation={3}
       sx={{
         p: 2,
-        gap: 2,
         minHeight: '60dvh',
         width: { md: '100%' } 
       }}
     >
-      {tasks.map(task => (
-        <Box sx={{ bgcolor: red[500] }}>
-          {task.title}
-        </Box>
-      ))}
+      <List sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+        {tasks.map((task, index) => (
+          <ListItemButton
+            key={index}
+            sx={{
+              bgcolor: red[500],
+              color: 'white',
+              p: 2,
+
+              '&:hover': {
+                bgcolor: red[400]
+              }
+            }}
+          >
+            <ListItemAvatar>
+              <Avatar sx={{ bgcolor: red[700] }}>
+                <PendingIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText
+              primary={task.title}
+              primaryTypographyProps={{
+                noWrap: true,
+                style: { overflow: 'hidden', textOverflow: 'ellipsis' }
+              }}
+            >
+              {task.title}
+            </ListItemText>
+          </ListItemButton>
+        ))}
+      </List>
     </Paper>
   )
 }
